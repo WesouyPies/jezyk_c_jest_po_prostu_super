cc5da7c8b9f54ed97db58d1e98093aeec469724f
HendoKundo <97247824+HendoKundo@users.noreply.github.com>
2022-01-12 20:27:20 +0100

    Add files via upload

---
+++ zadanie1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+int main(){
+    int wordcount = 1;  //liczy ilość spacje wiec jeżeli wprwowadzilibyśmy jedno słowo to by pokazało że jest zero słów
+    int numchar = 0;    //ilość zanaków
+    char character;
+    while((character=getchar())!='\n'){     //wyczytujemy znak
+        if(!isspace(character))     //jeżeli char różny od spacji zwiększamy ilość znaków
+            numchar++;
+        if(isspace(character))      //jeżeli zanak to spacja zwiększmy ilość słów
+            wordcount++;
+    }
+    printf("liczba znakow wynosi %d, liczba slow wynosi %d", numchar, wordcount);
+    return 0;
+}
