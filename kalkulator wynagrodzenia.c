7c3ee9b65a938aa80380a49fc14a5e55ccaf192b
UR5K4CZM4R3K <97252909+UR5K4CZM4R3K@users.noreply.github.com>
2022-01-12 21:03:12 +0100

    Update kalkulator wynagrodzenia.c

--- kalkulator wynagrodzenia.c
+++ kalkulator wynagrodzenia.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
 
-#define WYNAGRODZENIE 40   //definiujemy stałe jak wynagrodzenie, standardową ilość godzin i stopy poddatkowe
+#define WYNAGRODZENIE 40   //definiujemy stałe jak wynagrodzenie, standardową ilość godzin i stopy poddatkowe.
 #define PODGODZIN 40
 #define POD1200 0.15
 #define POD600 0.2
 #define PODRESZTA 0.25
 
-int salary(int hours) {  //liczymy pensje
-    if (hours <= PODGODZIN)    //sprawdzamy czy ilość godzi kwalifikuje na nadgodziny
-        return hours * WYNAGRODZENIE;   //jeżeli mniej lub równa 40 to po prostu liczymy wynagdrodzenie
+int salary(int hours) {  //liczymy pensje.
+    if (hours <= PODGODZIN)    //sprawdzamy czy ilość godzi kwalifikuje na nadgodziny.
+        return hours * WYNAGRODZENIE;   //jeżeli mniej lub równa 40 to po prostu liczymy wynagdrodzenie.
     else {
-        int temp = hours - PODGODZIN;   //podajemy tymczasowego integera który odpowiada za ilość nadgodzin
+        int temp = hours - PODGODZIN;   //podajemy tymczasowego integera który odpowiada za ilość nadgodzin.
         return hours * WYNAGRODZENIE + (temp * WYNAGRODZENIE * 1.5);
     }
 }
 
-int calctax(int salary) {   //funkcja licząca podatek
+int calctax(int salary) {   //funkcja licząca podatek.
     int tax = 0;
     if(salary <= 1200)
-        tax=salary * POD1200;   //liczymy podatek jeżeli wynagrodzemie wynosi 1200 lub mniej
+        tax=salary * POD1200;   //liczymy podatek jeżeli wynagrodzemie wynosi 1200 lub mniej.
     else{
-        int temp = salary - 1200;   //liczymy podatek dla pierwszy 1200
+        int temp = salary - 1200;   //liczymy podatek dla pierwszy 1200.
         tax = salary * POD1200;
-        if(temp <= 600 && temp>0){  //dla kolejnych 600
+        if(temp <= 600 && temp>0){  //dla kolejnych 600.
             tax += temp*POD600;
             temp-= 600;
-            if(temp > 0)        //dla reszty wynagrodzenia
+            if(temp > 0)        //dla reszty wynagrodzenia.
                 tax += temp * PODRESZTA;
         }
     }
@@ -36,7 +36,7 @@ int calctax(int salary) {   //funkcja licząca podatek
 int main() {
     int hours;
     printf("Wprowadz ilosc przepracowanych godzin\n");
-    scanf("%d", &hours);    //wprowadzamy ilość przepracowanych godzin
+    scanf("%d", &hours);    //wprowadzamy ilość przepracowanych godzin.
     printf("twoje wynagrodzenie to %d\ntwoj podatek do zaplacenia to %d", salary(hours),calctax(salary(hours)));
     return 0;
 }
