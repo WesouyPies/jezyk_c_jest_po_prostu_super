e87e5c0592e31c0a30e71b8a54a1d66734d59b8c
UR5K4CZM4R3K <97252909+UR5K4CZM4R3K@users.noreply.github.com>
2022-01-12 21:30:48 +0100

    Update decimal_to_binary.c

--- decimal_to_binary.c
+++ decimal_to_binary.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-
+//Urs Kaczmarek
 int main()
 {
     for (int character = 32; character < 256;) { //pierwsze 32 wartości char to liczby co nas nie interesuje, 256 to jeden więcej niż maksymalna wartość char
         for (int cpr = 0; cpr < 16; cpr++) {     // cpr- characters per row. zwiększamy wartiść char (przejscie do następnego znaku) aż podniesiemy cpr do 16
             printf("%c ", (char)character);
-            character++;
+            character++;                         //
         }
         printf("\n"); // przejście do następnego wiersza.
     }
